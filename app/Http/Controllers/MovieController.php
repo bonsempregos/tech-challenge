1f92e5698975c38599133a88740c3a7414745dc1
Pedro Sousa <pmcsousa@worten.pt>
2021-01-28 05:59:48 +0000

    EDD-00001

---
+++ app/Http/Controllers/MovieController.php
@@ -0,0 +1,82 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Controllers\Traits\HasFetchAllRenderCapabilities;
+use App\Http\Requests\MovieRequest;
+use App\Models\Movie;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\ResourceCollection;
+
+class MovieController extends Controller
+{
+
+    use HasFetchAllRenderCapabilities;
+
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Request $request)
+    {
+        $this->setGetAllBuilder(Movie::query());
+        $this->setGetAllOrdering('name', 'asc');
+        $this->parseRequestConditions($request);
+        return new ResourceCollection($this->getAll()->paginate());
+    }
+
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(MovieRequest $request)
+    {
+        $movie = new Movie($request->validated());
+        $movie->save();
+
+        return new \App\Http\Resources\Movie($movie);
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Movie $movie)
+    {
+        return new \App\Http\Resources\Movie($movie);
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Movie $movie, MovieRequest $request)
+    {
+        $movie->fill($request->validated());
+        $movie->save();
+
+        return new \App\Http\Resources\Movie($movie);
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Movie  $movie
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Movie $movie)
+    {
+        $movie->delete();
+
+        return response()->noContent();
+    }
+}
---
+++ database/migrations/2021_01_28_052743_create_movies_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateMoviesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('movies', function (Blueprint $table) {
+            $table->uuid('id')->primary();
+            $table->string('name')->unique()->index();
+            $table->year('year');
+            $table->text('synopsis');
+            $table->integer('minutes');
+            $table->date('released_at');
+            $table->integer('cost');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('movies');
+    }
+}
